c5494a857399caa56eba081d858809b8a245c1e6
Julian <angela.julian@gmail.com>
2021-05-16 17:38:49 -0700

    dealing with merge issues

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 // Assignment code here
-
 function generatePassword() {
   var password = "";
   let password_length = prompt(
@@ -29,6 +28,7 @@ function generatePassword() {
     );
 
     let yeses = [lower_case, upper_case, numbers, special_characters];
+    console.log(yeses);
     if (yeses.includes(true)) {
       var alphabet = "abcdefghijklmnopqrstuvwxyz";
       var password_array = [];
@@ -55,12 +55,8 @@ function generatePassword() {
         }
         if (special_characters && i < password_length) {
           i = i + 1;
-<<<<<<< HEAD:friendly-parakeet-main/Develop/script.js
-          var special = "~!@#$%^&*()_+`-=[]{};':|<>?/,." + '"';
-=======
           var special = "`~!@'#$.,%^&*()-_=+;:[{]}|?/<>" + '"';
 
->>>>>>> temp:assets/script.js
           // add special character to password_array
           password_array.push(
             special[Math.floor(Math.random() * special.length)]
@@ -69,11 +65,11 @@ function generatePassword() {
       }
       //scramble password_array & convert to string "password"
       password = password_array.join("");
-      return password;
     } else
       alert(
         "You must include at least one character set to generate a password."
       );
+    return password;
   }
 }
 
